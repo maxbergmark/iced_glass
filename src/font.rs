f44b2c8bb8269c1601e8e93af932b0f4467f7053
Max Bergmark <max.bergmark@gmail.com>
2026-04-29 10:03:49 +0200

    Change default font to Noto Sans

--- src/font.rs
+++ src/font.rs
@@ -1,12 +1,12 @@
-// pub const FONT: &[u8] = notosans::REGULAR_TTF;
-pub const FONT: &[u8] = include_bytes!("/System/Library/Fonts/Supplemental/Arial Unicode.ttf");
+pub const FONT: &[u8] = notosans::REGULAR_TTF;
+// pub const FONT: &[u8] = include_bytes!("/System/Library/Fonts/Supplemental/Arial Unicode.ttf");
 // pub const FONT: &[u8] = include_bytes!("/System/Library/Fonts/Hiragino Sans GB.ttc");
 // pub const FONT: &[u8] = include_bytes!("/System/Library/Fonts/Supplemental/Songti.ttc");
 // pub const FONT: &[u8] = include_bytes!("../assets/Plangothic.ttc");
 // pub const FONT: &[u8] = include_bytes!("/System/Library/Fonts/Supplemental/Thonburi.ttc");
 
-// pub const FAMILY: cosmic_text::Family = cosmic_text::Family::Name("Noto Sans");
-pub const FAMILY: cosmic_text::Family = cosmic_text::Family::Name("Arial Unicode MS");
+pub const FAMILY: cosmic_text::Family = cosmic_text::Family::Name("Noto Sans");
+// pub const FAMILY: cosmic_text::Family = cosmic_text::Family::Name("Arial Unicode MS");
 // pub const FAMILY: cosmic_text::Family = cosmic_text::Family::Name("Hiragino Sans GB");
 // pub const FAMILY: cosmic_text::Family = cosmic_text::Family::Name("Songti SC");
 // pub const FAMILY: cosmic_text::Family = cosmic_text::Family::Name("Plangothic");
